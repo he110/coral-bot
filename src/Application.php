69314659ee55915bcd386933e8e39e07d98ff7df
Ilya S. Zobenko <ilya@zobenko.ru>
2020-01-21 10:29:36 +0300

    Init base structure

---
+++ src/Application.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: he110
+ * Date: 2020-01-21
+ * Time: 10:21
+ */
+
+namespace He110\Coral\Bot;
+
+
+class Application
+{
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: he110
+ * Date: 2020-01-21
+ * Time: 10:29
+ */
+
+namespace He110\Coral\Bot\Entity;
+
+
+class User
+{
+
+}
